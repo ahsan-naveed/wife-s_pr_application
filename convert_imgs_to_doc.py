f4f1781e472d15309e8408e1578b14a57ac96be1
Ahsan Naveed <ahsannaveed007@gmail.com>
2023-03-16 20:27:45 -0700

    Photo annotator

---
+++ convert_imgs_to_doc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import docx
+import os
+
+from docx.shared import Inches
+
+rdir = '/Users/ahsannaveed/Desktop/Nimra PR Application/Nikah'
+cpics = 'Compressed Pics'
+apics = 'Annotated Pics'
+compressed_files = os.listdir(os.path.join(rdir, cpics))
+
+for i, f in enumerate(compressed_files):
+    # ignore hidden files
+    if not f.startswith('.'):
+        img_path = os.path.join(rdir, cpics, f)
+        doc = docx.Document()
+        doc.add_picture(img_path, width=Inches(6), height=Inches(4))
+        doc.add_paragraph(text='Name: Nimra Ahsan\nDOB: 1998-02-24\nContext: Nikah Ceremony\nLeft-Right:')
+        doc.save(os.path.join(rdir, apics, f'Ahsan-Nimra-Nikah-{i + 1}.docx'))
+
